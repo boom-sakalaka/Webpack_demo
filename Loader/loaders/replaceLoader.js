d19d922c84da12489cfda428fa69968bdb147cf3
Gu <936285702@qq.com>
2021-05-31 22:25:12 +0800

    loader 中异步的处理

--- Loader/loaders/replaceLoader.js
+++ Loader/loaders/replaceLoader.js
@@ -1,11 +1,20 @@
 /*
  * @Author: your name
  * @Date: 2021-05-31 22:04:25
- * @LastEditTime: 2021-05-31 22:10:10
+ * @LastEditTime: 2021-05-31 22:19:24
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \Webpack_demo\Loader\loaders\replaceLoader.js
  */
+
+const loaderUtils = require('loader-utils');
 module.exports = function (source) {
-  return source.replace('gzh', this.query.name);
+  const options = loaderUtils.getOptions(this);
+  // const result = source.replace('gzh', options.name);
+  // this.callback(null, result);
+  const callback = this.async();
+  setTimeout(function () {
+    const result = source.replace('gzh', options.name);
+    callback(null, result);
+  }, 1000);
 };
