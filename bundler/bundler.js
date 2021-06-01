46a8edb7d62781efeef2817a293d7fa56adc83ca
Gu <936285702@qq.com>
2021-06-01 23:06:22 +0800

    利用babel/core 生成浏览器可以运行的代码

--- bundler/bundler.js
+++ bundler/bundler.js
@@ -1,21 +1,42 @@
 /*
  * @Author: your name
  * @Date: 2021-06-01 21:14:22
- * @LastEditTime: 2021-06-01 21:29:25
+ * @LastEditTime: 2021-06-01 22:39:59
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \Webpack_demo\bundler\bundler.js
  */
 
 const fs = require('fs');
+const path = require('path');
 const paser = require('@babel/parser');
+const traverse = require('@babel/traverse').default;
+const babel = require('@babel/core');
+
 const moduleAnalyser = filename => {
   const content = fs.readFileSync(filename, 'utf-8');
   const ast = paser.parse(content, {
     sourceType: 'module',
   }); // 抽象语法树
-  console.log(ast.program.body);
-  // console.log(content);
+  const dependencies = {};
+  traverse(ast, {
+    ImportDeclaration({ node }) {
+      const dirname = path.dirname(filename);
+      const newFilr = path.join(dirname, node.source.value);
+      dependencies[node.source.value] = newFilr;
+    },
+  });
+  // console.log(dependencies);
+  const { code } = babel.transformFromAst(ast, null, {
+    presets: ['@babel/preset-env'],
+  });
+  //  console.log(code);
+  return {
+    filename,
+    dependencies,
+    code,
+  };
 };
 
-moduleAnalyser('./src/index.js');
+const moduleInfo = moduleAnalyser('./src/index.js');
+console.log(moduleInfo);
